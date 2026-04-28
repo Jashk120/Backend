9583f4774e8cda2a3c2a65965f33cb583796ad73
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-04-28 17:39:15 +0530

    docs: update src/utils/asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,10 @@
+```javascript
+/**
+ * Wraps an asynchronous request handler to catch any errors and pass them to the error-handling middleware.
+ *
+ * @param requestHandler - The asynchronous route handler function.
+ * @returns A new function that executes the request handler and catches any errors using `next(err)`.
+ */
 const asyncHandler =(requestHandler)=>{
     return  (req,res,next)=>{
        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
@@ -6,5 +13,4 @@ const asyncHandler =(requestHandler)=>{
 }
 
 export {asyncHandler}
-
-
+```

ce1ef6d6a133241b6c511caac9c702f2cbde7eaf
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-04-28 17:39:01 +0530

    docs: update src/db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,16 @@
+```javascript
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
+/**
+ * Establishes a connection to the MongoDB database using the URI from environment variables and the database name.
+ *
+ * Connects to the database and logs the host of the successful connection. If the connection fails, the error is logged
+ * and the Node.js process exits with code 1.
+ *
+ * @returns {Promise<void>} A promise that resolves when the connection is established.
+ * @throws Will exit the process if the connection fails.
+ */
 const connectDB= async () => {
     try{
         const connectionInstance= 
@@ -15,3 +24,4 @@ const connectDB= async () => {
 }
 
 export default connectDB
+```
